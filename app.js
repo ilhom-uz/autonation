f53632c5c5c175645d1945ba65c319407330680e
Ilhom Karimov <ilhom.uz@gmail.com>
2016-10-14 00:27:13 -0400

    Fully created static pages

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+var port = 3000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+//setting View engine for EJS
+app.set('views', './src/views');
+app.set('view engine', '.ejs');
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.get('/resume', function (req, res) {
+    var filePath = "/public/files/Ilkhom_Karimov_Resume.pdf";
+
+    fs.readFile(__dirname + filePath , function (err,data){
+        res.contentType("application/pdf");
+        res.send(data);
+    });
+});
+
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
+app.get('/contact', function (req, res) {
+    res.render('contact');
+});
+
+app.get('/portfolio', function (req, res) {
+    res.render('portfolio');
+});
+
+app.get('/blog', function (req, res) {
+    res.render('blog');
+});
+
+
+
+
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
+}  );
