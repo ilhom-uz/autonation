7d15baa3691d8abf8adde7206933976dc406b0c2
Ilhom Karimov <ilhom.uz@gmail.com>
2016-10-20 15:56:00 -0400

    Added Prototyping Article

--- app.js
+++ app.js
@@ -11,41 +11,8 @@ app.use(express.static('src/views'));
 app.set('views', './src/views');
 app.set('view engine', '.ejs');
 
-app.get('/', function (req, res) {
-    res.render('index');
-});
-
-app.get('/resume', function (req, res) {
-    var filePath = "/public/files/Ilkhom_Karimov_Resume.pdf";
-
-    fs.readFile(__dirname + filePath , function (err,data){
-        res.contentType("application/pdf");
-        res.send(data);
-    });
-});
-
-app.get('/about', function (req, res) {
-    res.render('about');
-});
-
-app.get('/contact', function (req, res) {
-    res.render('contact');
-});
-
-app.get('/portfolio', function (req, res) {
-    res.render('portfolio');
-});
-
-app.get('/blog', function (req, res) {
-    res.render('blog');
-});
-
-app.get('/portfolioproject', function (req, res) {
-    res.render('myportfolio');
-});
-
-
-
+//Routes from /routes/routes.js
+require('./routes')(app);
 
 
 app.listen(port, function (err) {
