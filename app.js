fff5864867f2aca5e60bc7e5f05b082b203b92ab
Ilkhom Karimov <ilkhom.karimov@outlook.com>
2016-12-02 23:57:55 -0500

    Port Name change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
--- routes/routes.js
+++ routes/routes.js
@@ -7,54 +7,6 @@ module.exports = function(app) {
         res.render('index');
     });
 
-    //Direct Link to my Resume
-    app.get('/resume', function (req, res) {
-        var filePath = "/../public/files/Ilkhom_Karimov_Resume.pdf";
-        fs.readFile(__dirname + filePath, function (err, data) {
-            res.contentType("application/pdf");
-            res.send(data);
-        });
-    });
-
-    //About me
-    app.get('/about', function (req, res) {
-        res.render('about');
-    });
-
-    //Contact me
-    app.get('/contact', function (req, res) {
-        res.render('contact');
-    });
-
-    // List of All portfolios
-    app.get('/portfolio', function (req, res) {
-        res.render('portfolio');
-    });
-
-    //Blog subdomain
-    app.get('/blog', function (req, res) {
-        res.render('blog');
-    });
-
-    //My Portfolio Project Description
-    app.get('/portfolioproject', function (req, res) {
-        res.render('myportfolio');
-    });
-
-    //Article Page about TweetyBitcoin Project
-    app.get('/tweetybitcoin', function (req, res) {
-        res.render('tweetybitcoin');
-    });
-
-    //Prototyping Page
-    app.get('/prototype', function (req, res) {
-        res.render('prototype');
-    });
-
-    //Smart Home
-    app.get('/smarthome', function (req, res) {
-        res.render('smarthome');
-    });
 
     //Smartup Ideas Page
     app.get('/startupidea', function (req, res) {
